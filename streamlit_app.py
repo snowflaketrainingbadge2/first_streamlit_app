e74f1ecf476d39b546f2b32807efc6c7a4b56f91
snowflaketrainingbadge2 <125675138+snowflaketrainingbadge2@users.noreply.github.com>
2023-02-21 10:59:59 -0500

    Update streamlit_app.py
    changed + kiwi to + fruit_choice

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,7 +28,7 @@ streamlit.write('The user entered ', fruit_choice)
 
 import requests
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 
 # take the json format of the text an normalize it 
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
